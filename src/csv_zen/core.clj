e4390c14747e21f244813303a7b2f08bf90fd82a
Eric Normand <ericwnormand@gmail.com>
2016-09-02 16:53:41 -0500

    Flesh out data model.

--- src/csv_zen/core.clj
+++ src/csv_zen/core.clj
@@ -7,8 +7,33 @@
 
 (hugsql/def-db-fns "csv_zen/database.sql")
 
+(defn init-db []
+  (install-uuid-module db)
+  (create-endpoint-table db)
+  (create-upload-table db)
+  (create-row-table db)
+  (create-cell-table db))
+
+(defn reset-db! []
+  (init-db)
+  (delete-all-cells db)
+  (delete-all-rows db)
+  (delete-all-uploads db)
+  (delete-all-endpoints db))
+
 (defn create-endpoint [db]
   (:id (create-endpoint* db)))
+
+(defn create-upload [db endpoint-id]
+  (:id (create-upload* db {:endpoint-id endpoint-id})))
+
+(defn create-row [db upload-id]
+  (:id (create-row* db {:upload-id upload-id})))
+
+(defn create-cell [db row-id key value]
+  (:id (create-cell* db {:row-id row-id
+                         :key key
+                         :value value})))
 
 (defn app [req]
   {:headers {}
--- src/csv_zen/database.sql
+++ src/csv_zen/database.sql
@@ -6,7 +6,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- :name create-endpoint-table
 -- :command :execute
 -- :result :raw
-create table endpoints (
+CREATE TABLE IF NOT EXISTS endpoints (
   id         UUID PRIMARY KEY,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 )
@@ -16,3 +16,57 @@ SELECT * FROM endpoints;
 
 -- :name create-endpoint* :<! :1
 INSERT INTO endpoints (id) VALUES (uuid_generate_v4()) RETURNING id;
+
+-- :name create-upload-table
+-- :command :execute
+-- :result :raw
+CREATE TABLE IF NOT EXISTS uploads (
+  id         UUID PRIMARY KEY,
+  endpoint_id UUID NOT NULL REFERENCES endpoints (id),
+  uploaded_at TIMESTAMPTZ NOT NULL DEFAULT now()
+)
+
+-- :name create-upload* :<! :1
+INSERT INTO uploads (id, endpoint_id)
+VALUES (uuid_generate_v4(), :endpoint-id)
+RETURNING id;
+
+-- :name create-row-table
+-- :command :execute
+-- :result :raw
+CREATE TABLE IF NOT EXISTS rows (
+  id         UUID PRIMARY KEY,
+  upload_id UUID NOT NULL REFERENCES uploads (id)
+)
+
+-- :name create-row* :<! :1
+INSERT INTO rows (id, upload_id)
+VALUES (uuid_generate_v4(), :upload-id)
+RETURNING id;
+
+-- :name create-cell-table
+-- :command :execute
+-- :result :raw
+CREATE TABLE IF NOT EXISTS cells (
+  id         UUID PRIMARY KEY,
+  row_id UUID NOT NULL REFERENCES rows (id),
+  key TEXT NOT NULL,
+  value TEXT NOT NULL
+)
+
+-- :name create-cell* :<! :1
+INSERT INTO cells (id, row_id, key, value)
+VALUES (uuid_generate_v4(), :row-id, :key, :value)
+RETURNING id;
+
+-- :name delete-all-endpoints :!
+DELETE FROM endpoints
+
+-- :name delete-all-uploads :!
+DELETE FROM uploads
+
+-- :name delete-all-rows :!
+DELETE FROM rows
+
+-- :name delete-all-cells :!
+DELETE FROM cells
